6f6502c931bd760103f89359fd9fafe97eb75960
Josef Lange <josef.d.lange@me.com>
2013-03-13 17:44:06 -0700

    Added iOS client documentation and build shell script for it.

---
+++ iOS Client Documentation/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+appledoc --project-name Expresso --project-company "Josef Lange & Daniel Guilak" --company-id edu.pugetsound.langeguilak --no-create-docset --output ./build --ignore ./MBProgressHUD.h --ignore ./MBProgressHUD.m --ignore ./ASIHTTPRequest ~/Projects/Expresso/expresso/Expresso/Expresso/Classes
