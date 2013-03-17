2cd2a4516f06c100b798a87c985c847941d10a2f
Josef Lange <josef.d.lange@me.com>
2013-03-16 22:00:31 -0700

    Removed old directory.

--- iOS Client Documentation/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ~/Projects/Expresso/expresso/Expresso/Expresso/Classes
-appledoc --project-name Expresso --project-company "Josef Lange & Daniel Guilak" --company-id edu.pugetsound.langeguilak --no-create-docset --output ./build --ignore ./MBProgressHUD.h --ignore ./MBProgressHUD.m --ignore ./ASIHTTPRequest .
